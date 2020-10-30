a0592bb8efdc6945e02039bea01a2acb9634103d
Norbu Samphel <norbusamphel@Norbus-MacBook-Pro.local>
2020-10-30 16:58:52 -0500

    npm install redux packages. Set up reduxstore in index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+//redux
+import logger from 'redux-logger';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const storeInstance = createStore(
+    combineReducers({
+        
+    }),
+    applyMiddleware(logger),
+);
+
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
