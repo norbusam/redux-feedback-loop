fecad4a9bba1e91ef01963619d57fd57e6309c65
Norbu Samphel <norbusamphel@Norbus-MacBook-Pro.local>
2020-10-31 15:22:02 -0500

    form validation on submit

--- src/components/FeelingFeedback/FeelingFeedback.js
+++ src/components/FeelingFeedback/FeelingFeedback.js
@@ -8,9 +8,7 @@ class FeelingFeedback extends Component {
     }
     // grabs the value and set the state to the value
     handleChange = (event) => {
-        // if(event.target.value === ''){
-        //     alert('Please submit a value between 1-5')
-        // }
+    
         console.log(event.target.value);
         this.setState({
             feeling: event.target.value
@@ -18,21 +16,22 @@ class FeelingFeedback extends Component {
     }// end handleChange
 
     submitAndNext = () =>{
-        // if(event.target.value === ''){
-        //     alert('Please submit a value between 1-5')
-        // }else {
-        //     console.log('added');
-        // }
-        this.props.dispatch({type:"ADD_FEEDBACK", payload: this.state.feeling})
-        this.props.history.push('/content')
+        if(this.state.feeling === 0 || this.state.feeling === ''){
+            alert('Please submit a value between 1-5')
+        }else {
+            console.log('added');
+            this.props.dispatch({type:"ADD_FEEDBACK", payload: this.state.feeling})
+            this.props.history.push('/content')
+        }
+       
     }
 
     render(){
         return(
             <div>
                 <h1>How are you feeling today?</h1>
-                <span>Feeling?</span>
-                <input type="number" min="0" max="5" onChange={this.handleChange}/>
+                <label html>Feeling?</label>
+                <input type="number" min="1" max="5" onChange={this.handleChange}/>
                 <button onClick={this.submitAndNext}>Next</button>
             </div>
         )
